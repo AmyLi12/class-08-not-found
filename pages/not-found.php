446339e02a6eae650ce3bc8edfc93e4e1f0e6b42
Amy Yi Li <142811190+AmyLi12@users.noreply.github.com>
2024-02-14 16:31:51 +0000

    class activity

---
+++ pages/not-found.php
@@ -0,0 +1,28 @@
+<?php
+$page_title = 'Error';
+
+$nav_home_class = '';
+$nav_citations_class = '';
+$nav_cooking_class = '';
+$nav_flowershop_class = '';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include 'includes/meta.php' ?>
+
+<body>
+
+    <?php include 'includes/header.php' ?>
+
+    <main>
+    <h2>Page Not Found</h2>
+
+    <p>Sorry, we couldn't find the page that you were trying to find. Try using the navigation bar to go to another page. </p>
+
+    </main>
+
+  <?php include 'includes/footer.php' ?>
+</body>
+
+</html>
